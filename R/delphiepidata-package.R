4f0734805d7e1f6c3837a5400ecf1402ce4c7f6f
boB Rudis <bob@rud.is>
2019-05-29 07:32:22 -0400

    R package repo initialization complete

---
+++ R/delphiepidata-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/delphiepidata>
+#' - BugReports: <https://gitlab.com/hrbrmstr/delphiepidata/issues>
+#' 
+#' @md
+#' @name delphiepidata
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
